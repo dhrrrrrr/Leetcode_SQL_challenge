312940109fc3d29846cc55c3e5ff0df566ed5766
dhrrrrrr <79271045+dhrrrrrr@users.noreply.github.com>
2023-06-12 17:15:06 -0600

    Create 550. Game Play Analysis IV.sql

---
+++ Medium/550. Game Play Analysis IV.sql
@@ -0,0 +1,62 @@
+-- Table: Activity
+-- 
+-- +--------------+---------+
+-- | Column Name  | Type    |
+-- +--------------+---------+
+-- | player_id    | int     |
+-- | device_id    | int     |
+-- | event_date   | date    |
+-- | games_played | int     |
+-- +--------------+---------+
+-- (player_id, event_date) is the primary key of this table.
+-- This table shows the activity of players of some games.
+-- Each row is a record of a player who logged in and played a number of games (possibly 0) before logging out on someday using some device.
+--  
+-- 
+-- Write an SQL query to report the fraction of players that logged in again on the day after the day they first logged in, rounded to 2 decimal places. In other words, you need to count the number of players that logged in for at least two consecutive days starting from their first login date, then divide that number by the total number of players.
+-- 
+-- The query result format is in the following example.
+-- 
+--  
+-- 
+-- Example 1:
+-- 
+-- Input: 
+-- Activity table:
+-- +-----------+-----------+------------+--------------+
+-- | player_id | device_id | event_date | games_played |
+-- +-----------+-----------+------------+--------------+
+-- | 1         | 2         | 2016-03-01 | 5            |
+-- | 1         | 2         | 2016-03-02 | 6            |
+-- | 2         | 3         | 2017-06-25 | 1            |
+-- | 3         | 1         | 2016-03-02 | 0            |
+-- | 3         | 4         | 2018-07-03 | 5            |
+-- +-----------+-----------+------------+--------------+
+-- Output: 
+-- +-----------+
+-- | fraction  |
+-- +-----------+
+-- | 0.33      |
+-- +-----------+
+-- Explanation: 
+-- Only the player with id 1 logged back in after the first day he had logged in so the answer is 1/3 = 0.33
+
+
+select
+    round(count(player_id)/(select count(distinct player_id) from activity),2) as fraction
+from
+    (
+        select 
+            player_id
+        from
+            (
+                select 
+                    player_id, 
+                    event_date,
+                    row_number() over (partition by player_id order by event_date) as rk,
+                    min(event_date) over (partition by player_id) as min_dt 
+                from activity
+            ) ranked
+        where
+            rk=2 and event_date-min_dt=1
+    )t;
